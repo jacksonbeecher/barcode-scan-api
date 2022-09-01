58312da25d97fe3a8f4ed8c35626ff552b5b014d
Jackson Beecher <jbeecher@ashcom.com.au>
2022-09-01 14:31:36 +1000

    Filter inactive users.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,13 @@
 const db = require('../models');
 //Get all users for user select login.
 const getAllUsers = async (req, res, next) => {
-    const users = await db.user.findAll({});
+    const users = await db.user.findAll({ where: { Active: true } });
     res.status(200).send(users);
 }
 //Get user to store user info in local storage. 
 const getUserById = async (req, res, next) => {
     let id = req.params.id;
-    const user = await db.user.findOne({ where: { UserId: id } });
+    const user = await db.user.findOne({ where: { UserId: id, Active: true } });
     res.status(200).send(user);
 }
 
